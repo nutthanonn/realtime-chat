749519c94bf5a6bb3372f5921ec148df5cbadfd6
nutthanon <nutthanon.tho@gmail.com>
2022-01-25 22:08:24 +0700

    update css

--- server/index.js
+++ server/index.js
@@ -2,7 +2,6 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const cors = require("cors");
-require("dotenv").config();
 const { Server } = require("socket.io");
 app.use(cors());
 
@@ -10,30 +9,28 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.PORT || "http://localhost:3000",
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
   },
 });
 
 io.on("connection", (socket) => {
-  console.log("User Connect");
+  console.log(`User Connected: ${socket.id}`);
 
   socket.on("join_room", (data) => {
     socket.join(data);
-    console.log(`User ID: ${socket.id} join room: ${data}`);
+    console.log(`User with ID: ${socket.id} joined room: ${data}`);
   });
-
-  // socket.on("receive_message", (data) => {});
 
   socket.on("send_message", (data) => {
     socket.to(data.room).emit("receive_message", data);
   });
 
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    console.log("User Disconnected", socket.id);
   });
 });
 
 server.listen(8000, () => {
-  console.log("Success");
+  console.log("SERVER RUNNING");
 });
